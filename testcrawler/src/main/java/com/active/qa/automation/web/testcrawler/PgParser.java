4bf4b16537750ffdf29c595877087c51f165c264
cheshiret <toa.chen@gmail.com>
2016-03-18 16:41:32 +0800

    1.now the the page could be cleaned as config whitelist 2. and the file could be saved as a single file as the title

--- testcrawler/src/main/java/com/active/qa/automation/web/testcrawler/PgParser.java
+++ testcrawler/src/main/java/com/active/qa/automation/web/testcrawler/PgParser.java
@@ -1,254 +1,148 @@
 package com.active.qa.automation.web.testcrawler;
 
-import com.active.qa.automation.web.testapi.TestApiConstants;
-import com.active.qa.automation.web.testapi.exception.NotSupportedException;
+import com.active.qa.automation.web.testapi.util.DateFunctions;
 import com.active.qa.automation.web.testapi.util.Property;
 import com.active.qa.automation.web.testapi.util.RegularExpression;
-import com.active.qa.automation.web.testapi.util.StringUtil;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.*;
+import org.jsoup.safety.Whitelist;
+import org.jsoup.select.Elements;
 import org.openqa.selenium.WebDriver;
 
-
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author : tchen
  * @since : 3/17/2016.
  */
 public class PgParser {
-  protected static final int NOTDEFINED=0;
-  protected static final int LINK=1;
-  protected static final int TEXT=2;
-  protected static final int LIST=3;
-  protected static final int CHECKBOX=4;
-  protected static final int RADIO=5;
-  protected static final int TABLE=6;
-  protected static final int FRAME=7;
-  protected static final int BUTTON=8;
-  protected static final int FRAMESET=9;
 
-  public static String getPageSource(WebDriver driver){
+  public static String getPageSource(WebDriver driver) {
     return driver.getPageSource();
   }
 
-  public static String[] getUrlText(String pagesource, String location){
-    Document doc = Jsoup.parseBodyFragment(pagesource);
-    ArrayList<String> list = new ArrayList<String>();
-    Element body = doc.body();
-    Elements contents = body.getElementsByClass(location);
-    for (Element content : contents) {
-      Elements links = content.getElementsByTag("a");
-      for (Element link : links) {
-//			  String linkHref = link.attr("href");
-        String linkText = link.text();
-        list.add(linkText);
-//			  System.out.println(linkHref+":"+linkText);
-      }
+  public static String getPgTitle(String pagesource) {
+    Document doc = Jsoup.parse(pagesource);
+    String title = doc.title();
+    if (!title.isEmpty()) {
+      return title;
+    } else {
+      return "NoPageTitle";
     }
-    return list.toArray(new String[0]);
 
   }
 
+  public static Elements getContents(String pagesource, String location) {
+    Document doc = Jsoup.parseBodyFragment(pagesource);
+    Elements contents;
+    Element body = doc.body();
+    if (location.isEmpty()) {
+      contents = body.getElementsByClass(location);
+    } else {
+      contents = body.getAllElements();
+    }
+    return contents;
+  }
+
+  public static String[] getTagText(String pagesource, String location, String tagName) {
+    Elements contents = getContents(pagesource, location);
+    ArrayList<String> list = new ArrayList<>();
+    for (Element content : contents) {
+      Elements tags = content.getElementsByTag(tagName);
+      for (Element tag : tags) {
+        String tagText = tag.text();
+        list.add(tagText);
+      }
+    }
+    return list.toArray(new String[0]);
+  }
+
+  public static String[] getAnchorUrlText(String pagesource, String location) {
+    return getTagText(pagesource, location, "a");
+  }
+
+  public static String[] getLabelText(String pagesource, String location) {
+    return getTagText(pagesource, location, "label");
+  }
+
+
+  public static String[] getDropdownOptions(String pagesource, String location) {
+    return getTagText(pagesource, location, "option");
+  }
+
+  public static String[] getUrlText(String pagesource) {
+    return getAnchorUrlText(pagesource, "");
+  }
+
+  public static String cleanbasic(String input) {
+    return Jsoup.clean(input, Whitelist.basic());
+  }
+
+  public static String cleanrelaxed(String input) {
+    return Jsoup.clean(input, Whitelist.relaxed());
+  }
+
+
+  public static String parsePg(String pagesource) {
+    Whitelist whitelist = new Whitelist();
+    whitelist.addAttributes("a", "href");
+    whitelist.addAttributes("input", "type", "class", "name", "id", "value");
+    whitelist.addAttributes("select", "name", "id");
+    return Jsoup.clean(pagesource, whitelist);
+  }
+
+  public static String pagesourceName(String pagesource) {
+    String name = null;
+    name = getPgTitle(pagesource).replaceAll("\\|*\\s+", "") + "_" +
+        DateFunctions.getLongDateStamp();
+    return name;
+  }
+
+  public static String gettable(String input) {
+    return Jsoup.clean(input, Whitelist.basic());
+  }
+
+  public static String getdiv(String input, int ind) {
+    return Jsoup.clean(input, Whitelist.basic());
+  }
+
+  public static String getform(String input, int ind) {
+    return Jsoup.clean(input, Whitelist.basic());
+  }
+
+  public static String gettext(String input, int ind) {
+    return Jsoup.clean(input, Whitelist.basic());
+  }
+
+  //id or name
+  public static String getcommonattr(String input) {
+    return Jsoup.clean(input, Whitelist.basic());
+  }
+
+  public void savePgSource(String pagesource) {
+    File file = new File("D:\\" + File.separator + this.pagesourceName(pagesource) + ".txt");
+    FileWriter fileWriter = null;
+    try {
+      fileWriter = new FileWriter(file, true);
+      //fileWriter.write("123");
+      fileWriter.write(parsePg(pagesource));
+      System.out.println("File was saved successfully!");
+      fileWriter.close();
+    } catch (IOException e) {
+      System.out.println("Error" + e);
+    }
+  }
 
   protected Property[] Property(String Parenttag, String tagName, String tag, RegularExpression regExp) {
-    return Property.toPropertyArray(Parenttag,tagName,tag,regExp);
+    return Property.toPropertyArray(Parenttag, tagName, tag, regExp);
   }
 
-  public static String[] getDropdownOptions(String pagesource, String location){
-    Document doc = Jsoup.parseBodyFragment(pagesource);
-    ArrayList<String> list = new ArrayList<String>();
-    Element body = doc.body();
-    Elements contents = body.getElementsByClass(location);
-    for (Element content : contents) {
-      Elements options = content.getElementsByTag("option");
-      for (Element option : options) {
-//			  String linkHref = link.attr("href");
-        String optionText = option.text();
-        list.add(optionText);
-//			  System.out.println(linkHref+":"+linkText);
-      }
-    }
-    return list.toArray(new String[0]);
-
-  }
-
-
-  public String cssSelector(Property[] properties){
-    String tag="*";
-    String text=null;
-    String id=null;
-    String className=null;
-    List<String> attr=new ArrayList<String>();
-
-    for(Property p:properties) {
-      String name=p.getPropertyName();
-      Object value=p.getPropertyValue();
-
-      if(name.equals(".class")) {
-        tag=getTagName((String)value);
-        if(tag.equalsIgnoreCase("input")) {
-          String type=null;
-          try {type=((String)value).substring(11).trim();} catch(Exception e){};
-          if(!StringUtil.isEmpty(type) ) {
-            if(type.equalsIgnoreCase("text"))
-              attr.add("[type!=hidden][type!=checkbox][type!=radio]");
-            else
-              attr.add("[type="+type+"]");
-          }
-        }
-      } else if(name.equals(".text")) {
-
-        if(value instanceof String) {
-          String t=(String) value;
-          if(t.contains(TestApiConstants.CELL_DELIMITER)) {
-            t=t.replaceAll(TestApiConstants.CELL_DELIMITER, " ");
-          }
-          text=":matches("+StringUtil.convertToRegex(t)+")";
-        } else {
-          text=":matches("+((RegularExpression)value).getJsoupPattern()+")";
-        }
-      } else if(name.equalsIgnoreCase(".id")){
-        if(value instanceof String) {
-          if(((String) value).contains(".") || ((String) value).contains("#") || ((String) value).contains(" ")) {
-            attr.add("[id="+value+"]");
-          } else {
-            id="#"+(String)value;
-          }
-        } else {
-          attr.add("[id~="+((RegularExpression)value).getJsoupPattern()+"]");
-        }
-
-      } else if(name.equalsIgnoreCase(".className")) {
-        if(value instanceof String) {
-          if(((String) value).contains(".") || ((String) value).contains("#") || ((String) value).contains(" ")) {
-            attr.add("[class="+(String)value+"]");
-          } else {
-            className="."+(String)value;
-          }
-        }else {
-          attr.add("[class~="+((RegularExpression)value).getJsoupPattern()+"]");
-        }
-      } else {
-        name=getAttributeName(name);
-        if(value instanceof String) {
-          attr.add("["+name+"="+(String)value+"]");
-        } else {
-          attr.add("["+name+"~="+((RegularExpression)value).getJsoupPattern()+"]");
-        }
-      }
-    }
-
-    StringBuffer buf=new StringBuffer();
-    buf.append(tag);
-    if(id!=null) {
-      buf.append(id);
-    }
-
-    if(className!=null) {
-      buf.append(className);
-    }
-
-    for(String s:attr) {
-      buf.append(s);
-    }
-
-    if(text!=null) {
-      buf.append(text);
-    }
-
-    String selector=buf.toString();
-//		logger.debug("CSS Selector="+selector);
-    return selector;
-  }
-
-
-  protected String getAttributeName(String value) {
-    if(value.equalsIgnoreCase(".className")) {
-      return "class";
-    } else {
-      if(value.startsWith("."))
-        return value.substring(1);
-      else
-        return value;
-    }
-  }
-
-  protected String getTagName(String value) {
-    String tagName="";
-    int i=value.indexOf(".");
-    int j=value.lastIndexOf(".");
-    if(i==j) {
-      tagName=value.substring(i+1);
-    } else {
-      tagName=value.substring(i+1,j);
-    }
-
-    return tagName;
-  }
-  protected int getTestObjectTypeFromProperty(Property[] property) {
-    for(int i=0;i<property.length;i++) {
-      if(property[i].getPropertyName().equalsIgnoreCase(".class")) {
-        String value=property[i].getPropertyValue().toString();
-        if(value.matches("Html.(INPUT.(text|password|time|date)|TEXTAREA)")){
-          return TEXT;
-        } else if(value.equalsIgnoreCase("Html.A")) {
-          return LINK;
-        } else if(value.equalsIgnoreCase("Html.TABLE")) {
-          return TABLE;
-        } else if(value.equalsIgnoreCase("Html.SELECT")) {
-          return LIST;
-        } else if(value.equalsIgnoreCase("Html.INPUT.checkbox")) {
-          return CHECKBOX;
-        } else if(value.equalsIgnoreCase("Html.INPUT.radio")) {
-          return RADIO;
-        } else if(value.equalsIgnoreCase("Html.FRAME")) {
-          return FRAME;
-        } else if(value.equalsIgnoreCase("Html.FRAMESET")) {
-          return FRAMESET;
-        } else if(value.equalsIgnoreCase("Html.INPUT")) {
-          throw new NotSupportedException("The type of INPUT is not specified. It should be Html.INPUT.<type>.");
-        }
-      }
-    }
-    return NOTDEFINED;
-  }
-
-
-//	public void getLable(Property[] p, String url){
-//
-//	}
-
-//	public void getTextField(Property[] p, String url){
-//
-//}
-
-//	public void getCheckbox(Property[] p, String url){
-//
-//}
-
-
-  public void getLable(String url){
-
-  }
-
-
-  public void getButtons(String url){
-
-  }
-
-  public void getType(String url){
-
-  }
-
-  public void compareHTML(String pagesource){
+  public void compareHTML(String pagesource) {
     //find element / div id etc.
   }
-  String pageContent=null;
-  public void PgParser(String pageContent) {
 
-  }
 }
--- testcrawler/src/main/java/com/active/qa/automation/web/testcrawler/testMain.java
+++ testcrawler/src/main/java/com/active/qa/automation/web/testcrawler/testMain.java
@@ -4,18 +4,26 @@ package com.active.qa.automation.web.testcrawler;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
+import java.io.File;
+
 /**
  * @author : tchen
  * @since : 3/17/2016.
  */
 public class testMain {
   public static void main(String[] args) throws Exception {
-    String url = "https://orms-torqa5.dev.activenetwork.com/";
+    String url = "https://orms-torqa5.dev.activenetwork.com/LicenseMgrLogin.do";
     System.setProperty("webdriver.ie.driver", "D:\\ShareFolder\\IEDriverServer_x64.exe");
     WebDriver driver = new InternetExplorerDriver();
     driver.get(url);
-    String source = PgParser.getPageSource(driver);
-    System.out.println( PgParser.cleanrelaxed(source));
+    PgParser ps = new PgParser();
+    String source = ps.getPageSource(driver);
+    ps.savePgSource(source);
     driver.close();
+
+    File ieDriver=new File(System.getProperty("webdriver.ie.driver"));
+    Runtime.getRuntime().exec("taskkill /F /IM "+ieDriver.getName()).waitFor();
+    System.gc();
+    System.exit(0);
   }
 }
